c4206abc9c1a16654bfa7f9d219172c141ebae2b
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-11-08 19:51:53 -0300

    Iniciando projeto

---
+++ src/main/java/com/api/incorporandoclasse/IncorporandoClasseApplication.java
@@ -0,0 +1,13 @@
+package com.api.incorporandoclasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncorporandoClasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncorporandoClasseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/incorporandoclasse/IncorporandoClasseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.incorporandoclasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncorporandoClasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
