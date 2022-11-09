89315f13db105d535adc3a0afcc5b0ce8b9f3dbc
claudiovdev <c.viniciusmagalhaes@gmail.com>
2022-11-08 22:06:39 -0300

    Adicionando primeiras regras na service

--- src/main/java/com/api/incorporandoclasse/services/Impl/UsuarioServiceImpl.java
+++ src/main/java/com/api/incorporandoclasse/services/Impl/UsuarioServiceImpl.java
@@ -1,10 +1,13 @@
 package com.api.incorporandoclasse.services.Impl;
 
+import com.api.incorporandoclasse.enums.StatusDoUsuario;
 import com.api.incorporandoclasse.models.UsuarioModel;
 import com.api.incorporandoclasse.repositories.UsuarioRepository;
 import com.api.incorporandoclasse.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -14,6 +17,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public UsuarioModel salvar(UsuarioModel usuario) {
+        usuario.setDataDeCriacao(LocalDate.now());
+        usuario.setDataDeAtualizacao(LocalDate.now());
+        usuario.setStatus(StatusDoUsuario.ATIVO);
         return repository.save(usuario);
     }
 }
